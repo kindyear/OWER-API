66f2193f79f91399fd588dcb21bf4bd9a9bff6e0
kindyear <kindyear@qq.com>
2023-07-24 20:50:25 +0800

    内建维护herosData.json英雄数据表

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 console.log(`Starting server...`);
 
-//  玩家基础信息
+// 英雄信息
+const herosData = require('./data/herosData.json');
+
+// 玩家基础信息
 const appPlayerInfo = require('./getPlayerInfo/playerInfo');
-//  玩家快速/竞技游戏英雄排行榜数据
+// 玩家快速/竞技游戏英雄排行榜数据
 const appPlayerPCQuickInfo = require('./getPlayerInfo/playerPCQuickInfo');
 const appPlayerPCCompetitiveInfo = require('./getPlayerInfo/playerPCCompetitiveInfo');
-//  玩家快速/竞技游戏英雄数据
+// 玩家快速/竞技游戏英雄数据
 const appPlayerPCQuickHerosInfo = require('./getPlayerInfo/playerPCQuickHerosInfo');
 const appPlayerPCCompetitiveHerosInfo = require('./getPlayerInfo/playerPCCompetitiveHerosInfo');
 /*
@@ -17,33 +20,34 @@ const appPlayerConsoleCompetitiveInfo = require('./getPlayerInfo/playerConsoleCo
 */
 const config = require('./config/config');
 const app = express();
-const {getCurrentTime} = require('./utils');
+const { getCurrentTime } = require('./utils');
 const PORT = config.PORT || 16524;
 const fs = require('fs');
-const {HOST} = require("./config/config");
+const { HOST } = require("./config/config");
+const heroIDData = require("./data/herosData.json");
 
-//  处理package.json文件
+// 处理package.json文件
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const version = packageJson.version;
 
-//  apiKey鉴权
+// apiKey鉴权
 function authenticate(req, res, next) {
-    const {apiKey} = req.query;
+    const { apiKey } = req.query;
 
     if (!apiKey || apiKey !== config.API_KEY) {
-        return res.status(401).json({error: 'Unauthorized. Please provide a valid API key.'});
+        return res.status(401).json({ error: 'Unauthorized. Please provide a valid API key.' });
     }
     next();
 }
 app.use(authenticate);
 
-//  玩家基础信息
+// 玩家基础信息
 app.get('/v1/api/playerInfo', async (req, res) => {
     try {
-        const {playerTag} = req.query;
+        const { playerTag } = req.query;
 
         if (!playerTag) {
-            return res.status(400).json({error: 'playerTag is required.'});
+            return res.status(400).json({ error: 'playerTag is required.' });
         }
 
         const playerData = await appPlayerInfo.playerInfo(playerTag);
@@ -52,14 +56,14 @@ app.get('/v1/api/playerInfo', async (req, res) => {
         console.log(`${getCurrentTime()} \u001b[33m${playerTag}\u001b[0m‘s data has been scraped successfully.`);
     } catch (error) {
         console.error(`${getCurrentTime()} Error:`, error.message);
-        res.status(500).json({error: 'Failed to scrape data.'});
+        res.status(500).json({ error: 'Failed to scrape data.' });
     }
 });
 
-//  PC平台玩家快速游戏信息
+// PC平台玩家快速游戏信息
 app.get('/v1/api/playerPCQuickInfo', async (req, res) => {
     try {
-        const {playerTag, type} = req.query;
+        const { playerTag, type } = req.query;
 
         // 在这里添加调试输出，确认type参数的值
         //console.log('Received request with playerTag:', playerTag, 'and type:', type);
@@ -68,11 +72,11 @@ app.get('/v1/api/playerPCQuickInfo', async (req, res) => {
         const selectedType = type ? type.toLowerCase() : null;
 
         if (!playerTag) {
-            return res.status(400).json({error: 'playerTag is required.'});
+            return res.status(400).json({ error: 'playerTag is required.' });
         }
 
         if (!selectedType || !(selectedType in appPlayerPCQuickInfo.typeToCategoryIdMap)) {
-            return res.status(400).json({error: 'Invalid type. Please provide a valid type for the rankings.'});
+            return res.status(400).json({ error: 'Invalid type. Please provide a valid type for the rankings.' });
         }
 
         const playerData = await appPlayerPCQuickInfo.scrapeHeroQuickPlayRankings(playerTag, selectedType);
@@ -81,27 +85,24 @@ app.get('/v1/api/playerPCQuickInfo', async (req, res) => {
         console.log(`${getCurrentTime()} \u001b[33m${playerTag}\u001b[0m‘s data has been scraped successfully.`); // 使用ANSI转义序列来设置playerTag为黄色
     } catch (error) {
         console.error(`${getCurrentTime()} Error:`, error.message);
-        res.status(500).json({error: 'Failed to scrape data.'});
+        res.status(500).json({ error: 'Failed to scrape data.' });
     }
 });
 
-//  PC平台玩家竞技比赛信息
+// PC平台玩家竞技比赛信息
 app.get('/v1/api/playerPCCompetitiveInfo', async (req, res) => {
     try {
-        const {playerTag, type} = req.query;
-
-        // 在这里添加调试输出，确认type参数的值
-        //console.log('Received request with playerTag:', playerTag, 'and type:', type);
+        const { playerTag, type } = req.query;
 
         // 将type参数转换为小写
         const selectedType = type ? type.toLowerCase() : null;
 
         if (!playerTag) {
-            return res.status(400).json({error: 'playerTag is required.'});
+            return res.status(400).json({ error: 'playerTag is required.' });
         }
 
         if (!selectedType || !(selectedType in appPlayerPCCompetitiveInfo.typeToCategoryIdMap)) {
-            return res.status(400).json({error: 'Invalid type. Please provide a valid type for the rankings.'});
+            return res.status(400).json({ error: 'Invalid type. Please provide a valid type for the rankings.' });
         }
 
         const playerData = await appPlayerPCCompetitiveInfo.scrapeHeroCompetitivePlayRankings(playerTag, selectedType);
@@ -110,23 +111,49 @@ app.get('/v1/api/playerPCCompetitiveInfo', async (req, res) => {
         console.log(`${getCurrentTime()} \u001b[33m${playerTag}\u001b[0m‘s data has been scraped successfully.`); // 使用ANSI转义序列来设置playerTag为黄色
     } catch (error) {
         console.error(`${getCurrentTime()} Error:`, error.message);
-        res.status(500).json({error: 'Failed to scrape data.'});
+        res.status(500).json({ error: 'Failed to scrape data.' });
     }
 });
 
-//  PC平台玩家快速游戏英雄数据
-app.get('/v1/api/playerPCQuickHerosInfo', async (req, res) => {});
+// PC平台玩家快速游戏英雄数据
+app.get('/v1/api/playerPCQuickHerosInfo', async (req, res) => { });
 
-//  PC平台玩家竞技比赛英雄数据
-app.get('/v1/api/playerPCCompetitiveHerosInfo', async (req, res) => {});
+// PC平台玩家竞技比赛英雄数据
+app.get('/v1/api/playerPCCompetitiveHerosInfo', async (req, res) => {
+    try {
+        const { playerTag, heroID } = req.query;
 
-//  主机平台玩家快速游戏信息（未开发）
+        // 在这里添加调试输出，确认传入的playerTag和heroID参数的值
+        //console.log('Received request with playerTag:', playerTag, 'and heroID:', heroID, heroName);
+
+        if (!playerTag || !heroID) {
+            return res.status(400).json({ error: 'playerTag and heroID are required.' });
+        }
+
+        if (!(heroID in herosData)) {
+            return res.status(400).json({ error: 'Invalid heroID. Please provide a valid heroID from the list of available heroes.' });
+        }
+        // 调用playerPCCompetitiveHerosInfo.js中的函数来处理玩家PC平台竞技模式的英雄信息
+        const playerData = await appPlayerPCCompetitiveHerosInfo.scrapeHeroCompetitiveInfo(playerTag, heroID);
+
+        // 返回处理后的数据给客户端
+        res.json(playerData);
+        console.log(`${getCurrentTime()} \u001b[33m${playerTag}\u001b[0m‘s competitive hero data for heroID \u001b[33m${heroID}\u001b[0m has been scraped successfully.`); // 使用ANSI转义序列来设置playerTag和heroID为黄色
+    } catch (error) {
+        console.error(`${getCurrentTime()} Error:`, error.message);
+        res.status(500).json({ error: 'Failed to scrape data.' });
+    }
+});
+
+
+
+// 主机平台玩家快速游戏信息（未开发）
 /*
 app.get('/v1/api/playerConsoleQuickInfo', async (req, res) => {
 
 });
 */
-//  主机平台玩家竞技比赛信息（未开发）
+// 主机平台玩家竞技比赛信息（未开发）
 /*app.get('/v1/api/playerConsoleCompetitiveInfo', async (req, res) => {
     res.json({message: '获取玩家竞技比赛信息'});
 });*/
--- getPlayerInfo/playerPCCompetitiveHerosInfo.js
+++ getPlayerInfo/playerPCCompetitiveHerosInfo.js
@@ -5,14 +5,70 @@
 
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const {getCurrentTime} = require('../utils');
-const {DATA_SOURCE} = require("../config/config");
+const { getCurrentTime } = require('../utils');
+const { DATA_SOURCE } = require("../config/config");
+const heroIDData = require('../data/herosData.json');
 
-async function scrapeHeroCompetitiveInfo(playerTag, heroName) {
-    const currentUNIXTime = new Date().getTime();
-    console.log(`${getCurrentTime()} Received API request for competitive hero info: \u001b[33m${playerTag}\u001b[0m heroName: \u001b[33m${heroName}\u001b[0m`);
-    const url = `${DATA_SOURCE}${encodeURIComponent(playerTag)}/`;
+async function scrapeHeroCompetitiveInfo(playerTag, heroID) {
+    try {
+        // 获取英雄名称和对应的原ID
+        const heroName = heroIDData[heroID].heroName;
+        const heroSourceID = heroIDData[heroID].heroSourceID;
 
+        const currentUNIXTime = new Date().getTime();
+        //console.log(`${getCurrentTime()} 收到对竞技模式英雄信息的API请求: \u001b[33m${playerTag}\u001b[0m 英雄ID: \u001b[33m${heroID}\u001b[0m`);
+        console.log(`${getCurrentTime()} Received request with playerTag: \u001b[33m${playerTag}\u001b[0m, heroID: \u001b[33m${heroID}\u001b[0m, heroName: \u001b[33m${heroName}\u001b[0m`);
+
+        const url = `${DATA_SOURCE}${encodeURIComponent(playerTag)}/`;
+
+        const browser = await puppeteer.launch({ headless: "new" });
+        const page = await browser.newPage();
+
+        // 开启请求拦截
+        await page.setRequestInterception(true);
+
+        // 请求拦截处理
+        page.on('request', (request) => {
+            // 阻止图片实际下载，但允许获取图片的 URL 信息
+            if (request.resourceType() === 'image') {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+
+        // 设置较长的超时时间（单位：毫秒）
+        await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 30000 });
+
+        // 玩家标签是否存在
+        const errorElement = await page.$('.error-contain');
+        if (errorElement) {
+            throw new Error(`未找到标签为 "${playerTag}" 的玩家。`);
+        }
+
+        // TODO: 添加逻辑从网页中爬取竞技模式英雄信息
+
+
+        await browser.close();
+
+        // 竞技模式英雄数据的占位符
+        const competitiveHeroData = {
+            // 在这里添加从网页中爬取的竞技模式英雄数据
+        };
+
+
+        return {
+            playerTag,
+            heroID,
+            heroName,
+            heroSourceID,
+            competitiveHeroData,
+            currentTime: currentUNIXTime
+        };
+    } catch (error) {
+        console.error(`${getCurrentTime()} Error:`, error.message);
+        throw new Error('Cannot get data.');
+    }
 }
 
-module.exports = {scrapeHeroCompetitiveInfo}
+module.exports = { scrapeHeroCompetitiveInfo };
